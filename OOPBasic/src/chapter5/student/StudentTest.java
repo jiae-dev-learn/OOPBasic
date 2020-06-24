e5fda8af56e28d432f654bae8c371ccfcc5c19da
jiae <jiae_kim@tmax.co.kr>
2020-06-24 20:51:20 +0900

    [chapter5] oop function, class

---
+++ OOPBasic/src/chapter5/student/StudentTest.java
@@ -0,0 +1,10 @@
+package chapter5.student;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student studentMinsoo = new Student(1001, "Minsoo", "Seoul");
+		studentMinsoo.showStudentInfo();
+		
+		
+	}
+}
