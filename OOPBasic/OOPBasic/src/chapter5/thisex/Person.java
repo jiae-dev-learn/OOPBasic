8e4c2d3cdd14a6ba91ff60f9da5fcd58fa72bc20
jiae <hirlawldo413@gmail.com>
2020-07-02 13:28:13 +0900

    [chapter5] this keyword

---
+++ OOPBasic/OOPBasic/src/chapter5/thisex/Person.java
@@ -0,0 +1,21 @@
+package chapter5.thisex;
+
+public class Person {
+	private String name;
+	private int age;
+	
+	public Person() {
+		this("이름없음", 1);
+	}
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+		
+	}
+	
+	public void showInfo() {
+		System.out.println(name + "," + age);
+	}
+	
+}
