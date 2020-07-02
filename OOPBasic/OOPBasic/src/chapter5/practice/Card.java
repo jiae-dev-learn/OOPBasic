3984d93a82ae177e3aa0c98649fb6387c3b17c31
jiae <hirlawldo413@gmail.com>
2020-07-02 19:47:06 +0900

    [chapter5] singleton

---
+++ OOPBasic/OOPBasic/src/chapter5/practice/Card.java
@@ -0,0 +1,15 @@
+package chapter5.practice;
+
+public class Card {
+	private int cardNumber;
+	
+	public Card(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public int getCardNumber() {
+		return cardNumber;
+	}
+
+	
+}
