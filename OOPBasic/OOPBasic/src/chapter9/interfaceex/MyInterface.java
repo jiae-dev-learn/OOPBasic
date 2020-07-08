ed78fdf0c4eda7b88698d17090cbfa410b03b768
jiae <hirlawldo413@gmail.com>
2020-07-08 22:40:06 +0900

    [chapter9] interface - interface에서 여러 interface를 상속받을 수 있다 - 상속받은 interface를 implement할 경우, 상속받은 모든 interface의 메서드를 구현해야 한다.

---
+++ OOPBasic/OOPBasic/src/chapter9/interfaceex/MyInterface.java
@@ -0,0 +1,5 @@
+package chapter9.interfaceex;
+
+public interface MyInterface extends X, Y {
+	void myMethod();
+}
---
+++ OOPBasic/OOPBasic/src/chapter9/interfaceex/Y.java
@@ -0,0 +1,5 @@
+package chapter9.interfaceex;
+
+public interface Y {
+	public void y();
+}
