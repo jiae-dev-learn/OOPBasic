bd281d9c717dffbe137cc2773e3e6c1b77e4e3b9
jiae <hirlawldo413@gmail.com>
2020-07-08 18:46:14 +0900

    [chapter9] interface - interface의 static 메서드 - interface의 private method - interface의 default 메서드

--- OOPBasic/OOPBasic/src/chapter9/interfaceex/Calc.java
+++ OOPBasic/OOPBasic/src/chapter9/interfaceex/Calc.java
@@ -8,5 +8,37 @@ public interface Calc {
 	int substract(int num1, int num2);
 	int times(int num1, int num2);
 	int divide(int num1, int num2);
+
+	default void description() {
+		System.out.println("점수 계산기를 구현합니다.");
+//		myMethod();
+	}
 	
+	/*
+	 * array의 합을 계산해서 반환하는 메서드
+	 * 추상메서드로만 제공이 되면 각각 구현을 해야 하고 default로 제공되면 인스턴스를 생성해서 만들어야 함.
+	 * 따라서 인스턴스를 생성하지 않고 유틸처럼 사용할 수 있는 기능은 인터페이스에서도 static으로 제공할 수 있음.
+	 */
+	static int total(int[] arr){
+		int total = 0;
+		
+		for(int i: arr) {
+			total += i;
+		}
+//		mystaticMethod();
+		return total;
+	}
+	
+	/*
+	 * private 메서드는 java9부터 제공한다.
+	 * 사용할 경우 JRE 버전을 확인한다.
+	 * 현재 1.8(무료)을 사용하고 있으므로 실습은 넘어간다.
+	 */
+//	private void myMethod() {
+//		System.out.println("private 메서드");
+//	}
+	
+//	private static void mystaticMethod() {
+//		System.out.println("private static method");
+//	}
 }
--- OOPBasic/OOPBasic/src/chapter9/interfaceex/CalcTest.java
+++ OOPBasic/OOPBasic/src/chapter9/interfaceex/CalcTest.java
@@ -3,8 +3,6 @@ package chapter9.interfaceex;
 public class CalcTest {
 
 	public static void main(String[] args) {
-		// 인터페이스를 구현한 클래스라고 하더라도, 
-		// 인터페이스에 포함되지 않은  추가된 메서드는 사용할 수 없다.
 		Calc calc = new CompleteCalc(); 
 		int num1 = 10;
 		int num2 = 2;
@@ -14,7 +12,14 @@ public class CalcTest {
 		System.out.println(calc.times(num1, num2));
 		System.out.println(calc.divide(num1, num2));
 
-//		calc.showInfo();
+		/*
+		 *  인터페이스를 구현한 클래스라고 하더라도
+		 *  인터페이스에 포함되지 않은  추가된 메서드는 사용할 수 없다. 
+		 */
+		//calc.showInfo();
+		calc.description();
+		int[] arr = {1,2,3,4,5};
+		System.out.println("배열의 합은 "+Calc.total(arr)+"입니다.");
 	}
 
 }
